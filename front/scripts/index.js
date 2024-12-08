f835b3a23b4daf42f4889c2aa65d126415a00afb
Macarena <macarena.aliberti@hotmail.com.ar>
2024-12-07 22:10:33 -0300

    Created initial HTML, CSS, and JavaScript files for movie project

--- front/scripts/index.js
+++ front/scripts/index.js
@@ -1,1 +1,28 @@
-console.log(tempData);
+const movieToHtml = (movie) => {
+    const container = document.createElement("div"); //crear const para q la funcion transforme las movies en html 
+    const image = document.createElement("img");
+    const title = document.createElement("h3");
+    const button = document.createElement("button");
+    
+    container.classList.add("movie-card");
+    image.src = movie.poster;
+    image.alt = movie.title;
+    title.innerText = movie.title;
+    button.innerText = "Mas informacion";
+
+    container.append(image, title, button);
+
+    return container;
+};
+
+const addToHTML = () => {
+    const movieCardsContainer = document.getElementById("movieCardsContainer"); //Pedirlo mediante el DOM
+
+    const movieElements = tempData.map(movieToHtml);
+
+    movieElements.forEach((movieElem) => movieCardsContainer.appendChild(movieElem)); 
+        
+};
+addToHTML();
+
+
