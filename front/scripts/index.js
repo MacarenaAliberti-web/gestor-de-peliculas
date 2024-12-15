41734c6a8000efc007bf0230f452096033b6ddc4
Macarena <macarena.aliberti@hotmail.com.ar>
2024-12-15 12:45:13 -0300

    Initial Bootstrap setup and custom styles

--- front/scripts/index.js
+++ front/scripts/index.js
@@ -1,18 +1,32 @@
 const movieToHtml = (movie) => {
-    const container = document.createElement("div"); //crear const para q la funcion transforme las movies en html 
+    const container = document.createElement("div"); 
+    container.classList.add("card", "bg-transparent", "border-0", "text-center");
+   
     const image = document.createElement("img");
-    const title = document.createElement("h3");
-    const button = document.createElement("button");
-    
-    container.classList.add("movie-card");
     image.src = movie.poster;
     image.alt = movie.title;
+    image.classList.add("card-img-top");
+
+    const title = document.createElement("h3");
     title.innerText = movie.title;
+    title.classList.add("card-title", "text-light")
+
+    const button = document.createElement("button");
     button.innerText = "Mas informacion";
+    button.classList.add("btn", "btn-outline-primary", "btn-sm");
+    
+    const cardBody = document.createElement("div");
+    cardBody.classList.add("card-body");
 
-    container.append(image, title, button);
+    cardBody.append(title, button);
+    container.append(image, cardBody);
 
-    return container;
+    const colContainer = document.createElement("div");
+    colContainer.classList.add("col");
+
+    colContainer.appendChild(container);
+
+    return colContainer;
 };
 
 const addToHTML = (data) => {
