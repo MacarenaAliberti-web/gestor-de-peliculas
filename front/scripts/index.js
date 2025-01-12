7fbc901fa38215aa5161997c61cb6c96b9dc349f
Macarena <macarena.aliberti@hotmail.com.ar>
2025-01-12 15:19:41 -0300

    Use Axios library and implement Async/Await

--- front/scripts/index.js
+++ front/scripts/index.js
@@ -1,11 +1,50 @@
 const addToHTML = require("./renderCards");
+const axios = require("axios");
 
-const getDataMovie = () => {
-    $.get("https://students-api.up.railway.app/movies", (data, status) => {
-    addToHTML(data);
+//const getDataMovie = () => {
+  //  $.get("https://students-api.up.railway.app/movies", (data, status) => {
+    //addToHTML(data);
+   // });
+//};
 
-    });
+// THEN Y CATCH
+//axios
+//.get("https://students-api.up.railway.app/movies")
+//.then((res) => {
+ //   addToHTML(res.data);
+//})
+//.catch((err) => {
+ //   console.error(err);
+//})
+//.finally(() =>{
+// console.log("Finalizo la llamada a la api");
+//});
+
+//ASYNC AWAIT
+const getDataMovie = async () => {
+    try{
+    const res = await axios.get("https://students-api.up.railway.app/movies");
+    addToHTML(res.data);
+    } catch (error) {
+        console.error(error.message);
+    } finally {
+        console.log("Finalizo la llamada a la api");
+    } 
 };
+
 
 getDataMovie();
 
+//fetch("https://students-api.up.railway.app/movies").then((res) => {
+  //  res.json().then((data) => {
+    //    console.log(data);
+    //});
+//});
+
+const getDataWithFetch = async () => {
+    const res = await fetch("https://students-api.up.railway.app/movies");
+    const data = await res.json();
+    console.log(data);
+};
+
+getDataWithFetch();
